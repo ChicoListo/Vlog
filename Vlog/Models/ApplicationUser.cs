99d84904bd2c9ac75c3518e795d308cfed8f6fc5
HomePC <fernandoleonventas@gmail.com>
2023-07-14 14:32:56 -0600

    Add project files.

---
+++ Vlog/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Vlog.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? firstname { get; set; }
+        public string? lastname { get; set;}
+        // Relacion
+        public List<Posts>? Posts { get; set; }
+    }
+}
